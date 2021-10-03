b50dec90555cc2318c3a6e49eafa59a1352a34a0
PlayerNo123 <86039714+PlayerNo123@users.noreply.github.com>
2021-10-03 19:27:11 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,183 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The appearance of Date and Time depends upon the ____________ applied to the cell?",
+    answer: "Format",
+    options: [
+      "Design",
+      "Format",
+      "Layout",
+      "Setup"
+    ]
+  },
+    {
+    numb: 2,
+    question: " To turn on Overtype mode _________ key is to be pressed from keyboard?",
+    answer: "Insert",
+    options: [
+      "Delete",
+      "Insert",
+      "Shift",
+      "Enter"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following languages are machine dependent?",
+    answer: "Machine and Assembly Language",
+    options: [
+      "Machine and Assembly Language",
+      "Machine and Fourth Generation Language",
+      "Fourth Generation and High Level Language",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A program written in Assembly Language is called as __________?",
+    answer: "Source Program",
+    options: [
+      "Object Program",
+      "Object Code",
+      "Source Program",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: " ___________ is the shortcut key to undo the last action?",
+    answer: "Ctrl + Z",
+    options: [
+      "Ctrl + X",
+      "Ctrl + Z",
+      "Ctrl + A",
+      "Ctrl + C"
+    ]
+  },
+    {
+      numb: 6,
+      question: "Which function key is used to modify the contents within a cell?",
+      answer: "F2",
+      options: [
+        "F2",
+        "F1",
+        "F9",
+        "F4"
+      ]
+  },
+    {
+      numb: 7,
+      question: "Which of the following is not a feature of Fourth Generation Language?",
+      answer: "Machine dependent",
+      options: [
+        "Machine dependent",
+        "User friendly",
+        "High speed of execution",
+        "Application development tools"
+      ]
+  },
+    {
+    numb: 8,
+    question: "We can select adjacent rows/column in a worksheet by using _______key.",
+    answer: "Shift",
+    options: [
+      "Ctrl",
+      "Enter",
+      "Shift",
+      "Tab"
+    ]
+  },
+    {
+    numb: 9,
+    question: "________ key combination can be used to minimise a workbook window to an icon.",
+    answer: "Ctrl + F9",
+    options: [
+      "Shift + F9",
+      "Ctrl + F9",
+      "Shift + F10",
+      "Ctrl + F10"
+    ]
+  },
+    {
+    numb: 10,
+    question: " C++ is an example of __________ language",
+    answer: "High level",
+    options: [
+      "Machine",
+      "High level",
+      "Assembly",
+      "None of these"
+    ]
+  },
+    {
+    numb: 11,
+    question: "__________ release refers to a product that is still in development but released for testing purpose.",
+    answer: "Beta",
+    options: [
+      "Alpha",
+      "Beta",
+      "Gamma",
+      "Pie"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Which language is directly understood by computers and does not require to be translated?",
+    answer: "Machine language",
+    options: [
+      "Assembly language",
+      "Machine language",
+      "High level language",
+      "None of these"
+    ]
+  },
+    {
+    numb: 13,
+    question: " ___________ was the first interactive computer game.",
+    answer: "Spacewar",
+    options: [
+      "Minecraft",
+      "PubG",
+      "Firewatch",
+      "Spacewar"
+    ]
+  },
+    {
+    numb: 14,
+    question: "A programmer is using 0's and 1's to write the program. Which generation of computer languages is he/ she using ?",
+    answer: "First",
+    options: [
+      "First",
+      "Third",
+      "Fourth",
+      "None of these"
+    ]
+  },
+    {
+    numb: 15,
+    question: " The _________ and ___________ buttons are present on the Formula bar.",
+    answer: "Cancel , Enter",
+    options: [
+      "Cancel , Enter",
+      "Enter , Insert",
+      "Insert , Enter",
+      "Cancel , Insert"
+    ]
+  },
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 16,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
